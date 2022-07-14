376fc845e1392122c844ef620e8e72c1fb1f3e9d
Eugene Avdeev <ea.avdeev@gmail.com>
2022-07-14 05:27:05 +0300

    js popup open,close

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,46 @@
+'use strict'
+
 let page = document.querySelector('.page');
 
-// Находим форму в DOM
-let popupForm = page.querySelector('.popup'); // Воспользуйтесь методом querySelector()
-// кнопки
+// profile - кнопки
 let btnEdit = page.querySelector('.profile__btn-edit'); // кнопка редактировать
 let btnAdd = page.querySelector('.profile__btn-add');
-let btnSave = page.querySelector('.popup__btn-save');
-let btnClose = page.querySelector('.popup__btn-close');
 let btnLike = page.querySelectorAll('.element__btn-like');
-
+// profile - поля
 let profileName = page.querySelector('.profile__name');
 let profileJob = page.querySelector('.profile__job');
 
+// Находим POPUP форму в DOM
+let popupForm = page.querySelector('.popup'); // Воспользуйтесь методом querySelector()
+// POPUP Кнопки
+let btnSave = page.querySelector('.popup__btn-save');
+let btnClose = page.querySelector('.popup__btn-close');
 // Находим поля формы в DOM
 let inputName = page.querySelector('.popup__input_name'); // Воспользуйтесь инструментом .querySelector()
 let inputJob = page.querySelector('.popup__input_job'); // Воспользуйтесь инструментом .querySelector()
 
-console.log(typeof inputJob);
+function popupFormOpen() {
+  // Чтобы попап открывался, ДОБАВЛЯЙТЕ ЕМУ МОДИФИКАТОР popup_opened с одним-единственным правилом...????
+  // Попап должен открываться !!! по нажатию кнопки!!! «Редактировать» - btnEdit
+  // а закрываться — при клике по крестику в правом верхнем углу - btnClose
+  // ИЗМЕНЯТЬ ЗНАЧЕНИЕ СВОЙСТВА НА flex
 
-function popupFormOpen(onclick) {
-  // Попап должен открываться !!! по нажатию кнопки!!! «Редактировать»,
-  // а закрываться — при клике по крестику в правом верхнем углу:
-  //  alert('XXXX xxxx!');
+//  popupForm.classList.add('popup_opened');
+  page.querySelector('.popup').style.display = 'flex';
+
+  // add display: flex;
+  //   popupForm.removeAttribute('display');
+
+  // 2.Чтобы закрыть попап, удаляйте у него модификатор popup_opened
+  // 3. Отслеживайте клик по кнопке методом addEventListener.
 }
+//popupFormOpen();
+btnEdit.addEventListener('click', popupFormOpen);
 
-popupFormOpen();
-
-popupForm.addEventListener('button', popupFormOpen);
+function popupFormClose() {
+  page.querySelector('.popup').style.display = 'none';
+}
+btnClose.addEventListener('click', popupFormClose);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
