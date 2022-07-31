0ded137e767f847cce0651b60d845863a23955cd
Eugene Avdeev <ea.avdeev@gmail.com>
2022-07-31 23:31:13 +0300

    added markup popup imgs container

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const page = document.querySelector('.page');
 // edit Profile
 const popupEditNode = document.querySelector('.popup_type_edit'); // Находим POPUP-edit форму в DOM.
 const btnEditProfile = document.querySelector('.profile__btn-edit'); // кнопка редактировать
-const btnSave = document.querySelector('.popup__btn-save');
+const btnSave = document.querySelector('.btn_type_save-profile');
 const btnClose = document.querySelector('.popup__btn-close');
 let inputName = page.querySelector('.popup__input_type_name'); // Воспользуйтесь инструментом .querySelector()
 let inputJob = page.querySelector('.popup__input_type_job');
@@ -12,13 +12,14 @@ let profileNameNode = page.querySelector('.profile__name');
 let profileJobNode = page.querySelector('.profile__job');
 // add Place
 const btnAddPlace = document.querySelector('.profile__btn-addplace');
+const btnCreateSavePlace = document.querySelector('.btn_type_create-place'); // btn "сохранить/создать"
 const popupAddPlaceNode = document.querySelector('.popup_type_add-place'); // Находим POPUP-add форму в DOM.
 // let btnLike = page.querySelectorAll('.element__btn-like');
 
 // // Каждый попап сохраните в своей переменной и создайте функцию, которая будут принимать в качестве аргумента указание,
 // // какой именно попап надо открыть или закрыть, т.е. передавайте туда класс-модификатор из совета ранее.
 
-// -------------------------------------------------------
+// ---- обработчик закрытия по кнопке "Х" и клику на страницу ------
 function handlerClosePopupClick(evt) {
   const target = evt.target;
   const activePopup = document.querySelector('.popup_opened');
@@ -39,6 +40,7 @@ function closePopup(modal) {
   modal.classList.remove('popup_opened');
 }
 
+// -- обработчик кнопки edit
 function handlerButtonEditClick(evt) {
   evt.preventDefault();
   setPopupEditInputValue(); // вызв заполнение полей
@@ -56,34 +58,54 @@ function setEditNodeTextValue() {
   profileJobNode.textContent = inputJob.value; // Получаем значение полей inputName  и inputJob из свойства value. //  // Выберите элементы, куда должны быть вставлены значения полей.
 }
 
-// --------------------------------
+// --------- обрботчик кнопки "сохранить" в ноде Edit
 function handlerButtonSaveSubmitForm(evt) {
-  // Функция Сохранения popup__btn-save и «отправки» данных из строк формы профиля, хотя пока она никуда отправляться не будет
+  // Функция Сохранения btn_type_save-profile и «отправки» данных из строк формы профиля, хотя пока она никуда отправляться не будет
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки. О том, как это делать, расскажем позже.
   setEditNodeTextValue();
   closePopup(popupEditNode);
-  // popupEditNodeClose(); // ВОЗМОЖНО ВЕРНУТЬ  !!!!!!!!!!
 }
 popupEditNode.addEventListener('submit', handlerButtonSaveSubmitForm); // Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
 
-// ---------------
+// ------------------------------------------------------------------
+// --- обработчик кнопки "+" / добавить Место
 function handlerButtonAddPlaceClick(evt) {
-  // на кнопке "+"
   evt.preventDefault();
   openPopup(popupAddPlaceNode);
 }
-
-//-------------
-// кнопки
-btnEditProfile.addEventListener('click', handlerButtonEditClick);
-btnClose.addEventListener('click', handlerClosePopupClick); // слушатель клика повешен на кнопку "close"
-btnSave.addEventListener('click', handlerButtonSaveSubmitForm);
-
 btnAddPlace.addEventListener('click', handlerButtonAddPlaceClick); // слушатель клика повешен на кнопку "+" ("add")
 // btnAddPlace.addEventListener('click', () => openPopup(popupAddPlaceNode)); // слушатель клика повешен на кнопку "+" ("add")
+
+// ######## НАПИСАТЬ ДОБАВЛЕНИЕ КАРТОЧЕК
+
+// Сделайте так, чтобы при клике на «сохранить» 'btnSave'(.btn_type_save-profile) новая карточка попадала в начало контейнера с ними.
+// А диалоговое окно после добавления автоматически закрывалось.
+// Чтобы создавать новые карточки, добавьте обработчик событий submit.
+//Сделайте это аналогично прошлому спринту, в котором вы настраивали редактирование информации о пользователе.
+
+// --------- обрботчик кнопки "сохранить(submit)" в ноде Place
+
+function handlerButtonSaveSubmitPlaceForm(evt) {
+  // Функция Сохранения btn_type_save-profile и «отправки» данных из строк формы профиля, хотя пока она никуда отправляться не будет
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы. Так мы можем определить свою логику отправки. О том, как это делать, расскажем позже.
+
+  setEditNodeTextValue();
+  closePopup(popupAddPlaceNode);
+}
+btnCreateSavePlace.addEventListener('submit', handlerButtonSaveSubmitPlaceForm); // Прикрепляем обработчик к кнопке: будет следить за событием “submit” - «отправка»
+
+
+
+
+
+//-------- к н о п к и
+btnEditProfile.addEventListener('click', handlerButtonEditClick);
+btnSave.addEventListener('click', handlerButtonSaveSubmitForm);
+
+btnClose.addEventListener('click', handlerClosePopupClick); // слушатель клика повешен на кнопку "close"
 page.addEventListener('click', handlerClosePopupClick);
 
-// ФОРМА POPUP EDIT ---------------------------------------------------------------------------------------
+// ///// - ЧЕРНОВИК - \\\\\\ ФОРМА POPUP EDIT /////////// ----------------------------------------------------------
 // откр
 // function popupEditNodeOpen(modal) { // принимаем в кач-ве параметра ДОМ-элемент
 //   popupEditNode.classList.add('popup_opened'); // форме попап добавлен класс-модификатор открытия
@@ -146,7 +168,7 @@ const cardsList = document.querySelector(selectors.list); // выбрали сп
 // const cardTemplate = document.querySelector(selectors.template).content.querySelector(selectors.card); // (!)темплейт через св-во content, и children
 const cardTemplate = document.querySelector(selectors.template).content; //(!) выбираем темплейт через св-во content, и children
 
-const initialCards = [
+const INITIAL_CARDS = [
   {
     name: 'Архыз',
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
@@ -173,7 +195,7 @@ const initialCards = [
   },
 ];
 
-initialCards.forEach(function (element) {
+INITIAL_CARDS.forEach(function (element) {
   // перебираем массив
   const cardElement = cardTemplate.cloneNode(true); // клонируем содержимое тега <template> (через объявление переменной)
 
@@ -182,6 +204,24 @@ initialCards.forEach(function (element) {
 
   cardsList.append(cardElement); // в список на странице втавляем склонированный контент, со всеми св-вами отобранными выше
 });
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+// function renderCard(data, container, position = 'append') {
+//   const card = createCard(data); //node
+//   switch (position) {
+//     case 'append':
+//       return container.append(card);
+//     case 'prepend':
+//       return container.prepend(card);
+//     case 'before':
+//       return container.before(card);
+//     case 'after':
+//       return container.after(card);
+//     default:
+//       return;
+//   }
+//   // container.append(todo)
+// }
 
 // ##################################################################################################################
 // function createCard(data) {
@@ -261,8 +301,6 @@ initialCards.forEach(function (element) {
 //
 //
 //
-//
-//
 
 // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
@@ -288,4 +326,4 @@ initialCards.forEach(function (element) {
 
 // // const sectionElementsCards = page.querySelectorAll('.elements__list.element'); // карточки из секции elements ul>li
 
-// // sectionElementsCards.innerHTML = initialCards;
+// // sectionElementsCards.innerHTML = INITIAL_CARDS;
